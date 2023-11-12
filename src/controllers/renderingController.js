eacb23b5824544008bcd4a6df36a8be92224a628
Alexandru Stelea <alexandrustelea@yahoo.com>
2023-11-12 02:13:20 +0200

    Created design for nearly all user interface. Everything is static for the moment. Need to implement backend functionalities.

--- src/controllers/renderingController.js
+++ src/controllers/renderingController.js
@@ -12,4 +12,32 @@ renderController.exercises = (req, res) => {
   res.render("exercises");
 };
 
+renderController.workouts = (req, res) => {
+  res.render("workouts");
+};
+
+renderController.plans = (req, res) => {
+  res.render("plans");
+};
+
+renderController.new = (req, res) => {
+  res.render("new");
+};
+
+renderController.exercise = (req, res) => {
+  res.render("exercise");
+};
+
+renderController.workout = (req, res) => {
+  res.render("workout");
+};
+
+renderController.plan = (req, res) => {
+  res.render("plan");
+};
+
+renderController.contact = (req, res) => {
+  res.render("contact");
+};
+
 export default renderController;
--- src/routes/renderingRoutes.js
+++ src/routes/renderingRoutes.js
@@ -6,5 +6,12 @@ const mainRouter = express.Router();
 mainRouter.get("/", renderController.landingPage);
 mainRouter.get("/index", renderController.index);
 mainRouter.get("/exercises", renderController.exercises);
+mainRouter.get("/workouts", renderController.workouts);
+mainRouter.get("/plans", renderController.plans);
+mainRouter.get("/new", renderController.new);
+mainRouter.get("/exercise", renderController.exercise);
+mainRouter.get("/workout", renderController.workout);
+mainRouter.get("/plan", renderController.plan);
+mainRouter.get("/contact", renderController.contact);
 
 export default mainRouter;
